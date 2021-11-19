55d69cac3bdae1d6391b957a4fa762dad9da426f
ratnairrinki1985 <93551616+ratnairrinki1985@users.noreply.github.com>
2021-11-19 22:17:31 +0100

    FirstCommit
    FirstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  loadedFeature = 'recipe';
+
+  OnNavigate(feature:string)
+  {
+    this.loadedFeature = feature;
+  }
+
+
 }
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe','testing the recipe entry','https://images.unsplash.com/photo-1615582583508-3999ef308a38?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
